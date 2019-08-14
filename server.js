4487c832af648cbb3ccf4344f6ee37fa3ef2282e
Kimberly <kdiaza@users.noreply.github.com>
2019-08-13 19:25:49 -0600

    adding configuration for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'); // Preguntar porque import express from 'express'; no sirve
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -12,7 +13,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-app.get('/', (req, res) => res.send('API Running'));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
